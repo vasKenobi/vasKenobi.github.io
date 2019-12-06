cf3080cc87885622d54e895914eb0021180fb71b
Vasyl Kle <vasyl.klets@gmail.com>
2019-12-06 23:50:26 +0200

    Updates

---
+++ precache-manifest.221d1c6a73888386cfb550d890a680bd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35968f6e33887b778c893757ff332568",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84a9b0ea0ce8861f9450",
+    "url": "/static/css/main.3c45b682.chunk.css"
+  },
+  {
+    "revision": "dd741769efa0150ea9b5",
+    "url": "/static/js/2.02549638.chunk.js"
+  },
+  {
+    "revision": "84a9b0ea0ce8861f9450",
+    "url": "/static/js/main.47575cb7.chunk.js"
+  },
+  {
+    "revision": "a192b83a9bc5da7b6fb8",
+    "url": "/static/js/runtime-main.ad904ba0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "74d9a801dbd0bcf4d2e44ffd4e3f8e23",
+    "url": "/static/media/logo.74d9a801.png"
+  }
+]);
